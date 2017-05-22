9792b86aa559a8e83581970e25a55f9e275e3d05
Daniel Romero <raiga1234@gmail.com>
2017-05-22 12:17:07 -0500

    Creando proyecto

---
+++ WebAPITorneoYa/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace WebAPITorneoYa.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
